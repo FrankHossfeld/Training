61939f8b3f6f10717a80ed6329bcfa351de6e6e2
Frank Hossfeld <frank.hossfeld@googlemail.com>
2017-10-23 21:57:48 +0200

    add module0703

---
+++ GWT-Grundlagen-Maven/module0703/src/main/java/de/gishmo/gwt/example/module0703/client/Module0703.java
@@ -0,0 +1,47 @@
+package de.gishmo.gwt.example.module0703.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.RootPanel;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class Module0703 implements EntryPoint {
+  
+  private DialogBox dialogBox;
+ 
+  /**
+   * This is the entry point method.
+   */
+  public void onModuleLoad() {
+    createDialogBox();
+    
+    final Button showButton = new Button("show dialogbox");
+    // We can add style names to widgets
+    showButton.addStyleName("sendButton");
+    
+    showButton.addClickHandler(new ClickHandler() {
+      @Override
+      public void onClick(ClickEvent event) {
+        dialogBox.center();
+      }
+    });
+
+    // Use RootPanel.get() to get the entire body element
+    RootPanel.get().add(showButton);
+
+  }
+
+  private void createDialogBox() {
+    dialogBox = new DialogBox();
+    dialogBox.setText("Kundensuche");
+    dialogBox.setAnimationEnabled(true);
+
+    // TODO: enter your code here
+
+  }
+}
