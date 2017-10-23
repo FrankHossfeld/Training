902f8808bad745b7b6f1d1cbcd3ade4ef73ac93a
Frank Hossfeld <frank.hossfeld@googlemail.com>
2017-10-23 22:21:58 +0200

    addd project module0707

---
+++ GWT-Grundlagen-Maven/module0707/src/main/java/de/gishmo/gwt/example/module0707/client/widgets/TextFieldStyle.java
@@ -0,0 +1,16 @@
+package de.gishmo.gwt.example.module0707.client.widgets;
+
+import com.google.gwt.resources.client.CssResource;
+
+public interface TextFieldStyle
+  extends CssResource {
+  
+  String panel();
+  
+  String label();
+  
+  String textBox();
+
+  String widgetPanel();
+  
+}
