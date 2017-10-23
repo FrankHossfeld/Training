74d86c81fd136f6c1cbb5107b6c2d1c1f1e01c32
Frank Hossfeld <frank.hossfeld@googlemail.com>
2017-10-23 23:42:42 +0200

    add püroject module0503

---
+++ GWT-Grundlagen-Maven/module0503/src/main/java/de/gishmo/gwt/example/module0503/client/PersonService.java
@@ -0,0 +1,26 @@
+package de.gishmo.gwt.example.module0503.client;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+import de.gishmo.gwt.example.module0503.shared.dto.Person;
+import de.gishmo.gwt.example.module0503.shared.dto.PersonSearch;
+import de.gishmo.gwt.example.module0503.shared.exception.PersonException;
+import de.gishmo.gwt.example.module0503.shared.exception.PersonNotFoundException;
+
+import java.util.List;
+
+@RemoteServiceRelativePath("person")
+public interface PersonService
+  extends RemoteService {
+  
+  Person get(long id) throws PersonNotFoundException;
+  
+  List<Person> getAll();
+  
+  List<Person> get(PersonSearch search);
+  
+  void insert(Person person) throws PersonException;
+  
+  void update(Person person) throws PersonException; 
+
+}
---
+++ GWT-Grundlagen-Maven/module0503/src/main/java/de/gishmo/gwt/example/module0503/client/PersonServiceAsync.java
@@ -0,0 +1,21 @@
+package de.gishmo.gwt.example.module0503.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import de.gishmo.gwt.example.module0503.shared.dto.Person;
+import de.gishmo.gwt.example.module0503.shared.dto.PersonSearch;
+
+import java.util.List;
+
+public interface PersonServiceAsync {
+
+  void get(long id, AsyncCallback<Person> callback);
+
+  void getAll(AsyncCallback<List<Person>> callback);
+
+  void get(PersonSearch search, AsyncCallback<List<Person>> callback);
+
+  void insert(Person person, AsyncCallback<Void> callback);
+
+  void update(Person person, AsyncCallback<Void> callback);
+
+}
