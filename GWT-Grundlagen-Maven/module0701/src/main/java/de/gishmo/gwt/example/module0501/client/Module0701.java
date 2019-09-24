f4f4c9ee1e2f27c8dc7908da1879e69a75b47661
Frank Hossfeld <frank.hossfeld@googlemail.com>
2019-09-24 09:41:58 +0200

    remove solution

--- GWT-Grundlagen-Maven/module0701/src/main/java/de/gishmo/gwt/example/module0501/client/Module0701.java
+++ GWT-Grundlagen-Maven/module0701/src/main/java/de/gishmo/gwt/example/module0501/client/Module0701.java
@@ -16,54 +16,6 @@ public class Module0701
    * This is the entry point method.
    */
   public void onModuleLoad() {
-    // einfaches Label
-    Label label = new Label("Ich bin ein Label!");
-    RootPanel.get()
-             .add(label);
-
-    // Label mit zugeordneter CSS-Style
-    Label labelHeadline = new Label("Ich bin ein Label mit " +
-                                    "dem Style Headline!");
-    labelHeadline.addStyleName("headline");
-    RootPanel.get()
-             .add(labelHeadline);
-
-
-    // einfaches HTML
-    HTML html = new HTML("<i>Kursiver Text</i>");
-    RootPanel.get()
-             .add(html);
-
-    // HTML mit setzen des Style-Attributes im DOM
-    HTML htmlKurUndBo = new HTML("Ich bin ein fettes, kurives HTML!");
-    htmlKurUndBo.getElement()
-                .setAttribute("style",
-                              "font-weight: bold;");
-    RootPanel.get()
-             .add(htmlKurUndBo);
-
-
-    Button button = new Button("Drück mich!");
-    button.setSize("200px",
-                   "30px");
-    button.addClickHandler(new ClickHandler() {
-      @Override
-      public void onClick(ClickEvent event) {
-        Window.alert("Ich wurde gedrückt!");
-      }
-    });
-    RootPanel.get()
-             .add(button);
-
-
-    Anchor anchor = new Anchor("Panik");
-    anchor.addClickHandler(new ClickHandler() {
-      @Override
-      public void onClick(ClickEvent event) {
-        Window.alert("Don't worry, be happy");
-      }
-    });
-    RootPanel.get()
-             .add(anchor);
   }
-}
+
+ }
